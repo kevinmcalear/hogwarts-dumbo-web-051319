92319372c658fa59161b6adc6fc63635164eb106
kevinmcalear <kevin.mcalear@gmail.com>
2019-07-22 15:49:16 -0400

    adds sort

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,12 @@ class App extends Component {
     if (this.state.showGreasedOnly) {
       filteredHogs = hogs.filter(hog => hog.greased)
     }
-    console.log('hog count', filteredHogs.length)
+
+    if (this.state.sortValue === 'name') {
+      filteredHogs = filteredHogs.sort((a, b) => a.name.localeCompare(b.name))
+    } else if (this.state.sortValue === 'weight') {
+      filteredHogs = filteredHogs.sort((a, b) => b['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'] - a['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'])
+    }
     const hogCards = filteredHogs.map(hog => <PigCard {...hog} />)
     return (
       <div className="App">
